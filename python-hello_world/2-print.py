83de8170aa4c8d443de2d1feb966efc4f720c0a1
SpeedCash <tcravero@gmail.com>
2024-01-22 13:39:13 +0000

    updated 2 print

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Write a Python script that prints exactly
-# "Programming is like building a multilingual puzzle,
-# followed by a new line.
 
 print("\"Programming is like building a multilingual puzzle\n")
