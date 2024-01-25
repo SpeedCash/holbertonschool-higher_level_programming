95ed456fcb3bb24958a27931cb6a30bd1f69c8b5
SpeedCash <tcravero@gmail.com>
2024-01-25 12:37:35 +0000

    Write a program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import importlib.util
+import sys
+
+
+def main():
+    curl_command = "curl -Lso "hidden_4.pyc" "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc"
+    os.system(curl_command)
+
+    module_name = "hidden_4"
+    module_spec = importlib.util.spec_from_file_location
+    (module_name, "./hidden_4.pyc")
+    module = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(module)
+
+    for name in dir(module):
+        if not name.startswith("__"):
+            print(name)
+
+
+if __name__ == "__main__":
+    main()
