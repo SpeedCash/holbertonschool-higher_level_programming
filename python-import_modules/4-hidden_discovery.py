9957dc358c3c860f9fc007414e002a912c4e243e
SpeedCash <tcravero@gmail.com>
2024-01-25 12:53:49 +0000

    Write a program that prints all the names defined by the compiled module hidden_4.pyc

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 import importlib.util
-import sys
+import os
 
 
 def main():
-    curl_command = "curl -Lso "hidden_4.pyc"
-    "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc""
+    # Télécharger le fichier .pyc
+    curl_command = "curl -Lso 'hidden_4.pyc' 'https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc'"
     os.system(curl_command)
 
+    # Charger le module compilé
     module_name = "hidden_4"
-    module_spec = importlib.util.spec_from_file_location
-    (module_name, "./hidden_4.pyc")
+    module_spec = importlib.util.spec_from_file_location(module_name, "./hidden_4.pyc")
     module = importlib.util.module_from_spec(module_spec)
     module_spec.loader.exec_module(module)
 
+    # Afficher les noms qui ne commencent pas par '__'
     for name in dir(module):
         if not name.startswith("__"):
             print(name)
