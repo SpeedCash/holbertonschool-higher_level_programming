0e295a08b981d0cc35079ba2e1d367bb58d87cf0
SpeedCash <tcravero@gmail.com>
2024-01-29 16:14:33 +0000

    updated new in list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,10 +6,3 @@ def new_in_list(my_list, idx, element):
     if 0 <= idx < len(list_copy):
         list_copy[idx] = element
     return list_copy
-
-
-if __name__ == "__main__":
-    ma_liste = [1, 2, 3, 4, 5]
-    nouvelle_liste = new_in_list(ma_liste, 2, 9)
-    print(nouvelle_liste)
-    print(ma_liste)
