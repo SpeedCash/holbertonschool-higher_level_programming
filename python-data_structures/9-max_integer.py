2d249a7ac326b8acc672251a5d42f4c585d1d20d
SpeedCash <tcravero@gmail.com>
2024-01-29 16:15:46 +0000

    updated max integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -11,11 +11,3 @@ def max_integer(my_list=[]):
             max_val = num
 
     return max_val
-
-
-if __name__ == "__main__":
-    liste = [1, 2, 3, 4, 5]
-    print(max_integer(liste))
-
-    liste_vide = []
-    print(max_integer(liste_vide))
