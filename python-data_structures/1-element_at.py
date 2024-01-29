ce29cf0681c2100b247869ab9c85674d2fa96fc2
SpeedCash <tcravero@gmail.com>
2024-01-29 16:08:23 +0000

    the element at

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,10 +5,3 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
-
-
-if __name__ == "__main__":
-    ma_liste = [1, 2, 3, 4, 5]
-    print(element_at(ma_liste, 2))
-    print(element_at(ma_liste, 5))
-    print(element_at(ma_liste, -1))
