65ac7fbe5c5b02f71517de6952a2a5e4c60ce4dd
SpeedCash <tcravero@gmail.com>
2024-01-29 16:14:45 +0000

    updated no cpy

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,8 +3,3 @@
 def no_c(my_string):
 
     return ''.join(char for char in my_string if char not in ['c', 'C'])
-
-
-if __name__ == "__main__":
-    chaine = "Concise Code in Python"
-    print(no_c(chaine))
