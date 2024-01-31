7a78ea8e8892cf1a69103300fe606012636b94bc
SpeedCash <tcravero@gmail.com>
2024-01-31 10:54:35 +0000

    the update dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,11 +2,13 @@
 
 def update_dictionary(a_dictionary, key, value):
 
-    if not key in a_dictionary:
+    if key not in a_dictionary:
         a_dictionary[key] = value
+    else:
 
-    else: 
         for index in a_dictionary:
             if index == key:
-                a_dictionary [index]= value
+
+                a_dictionary[index] = value
+
     return a_dictionary
