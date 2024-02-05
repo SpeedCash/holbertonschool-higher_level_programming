22be6b47144b1b06e909c65bc6ef26224c11bce0
SpeedCash <tcravero@gmail.com>
2024-02-05 14:00:34 +0000

    the 2 main file updated

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
+"""A class that defines a square by its size.
+
+Attributes:
+size (int): The size of a side of the square.
+
+Methods:
+__init__(self, size=0): Initializes a new square.
+area(self): Returns the current square area.
+
+Raises:
+TypeError: If `size` is not an integer.
+ValueError: If `size` is less than 0.
+"""
+
 
 class Square:
-    """A class that defines a square by its size.
 
-    Attributes:
-        size (int): The size of a side of the square.
 
-    Methods:
-        __init__(self, size=0): Initializes a new square.
-        area(self): Returns the current square area.
-
+    """Initialize a new Square.
+    Args:
+    size (int, optional): The size of the square's side. Defaults to 0.
     Raises:
-        TypeError: If `size` is not an integer.
-        ValueError: If `size` is less than 0.
+    TypeError: If `size` is not an integer.
+    ValueError: If `size` is less than 0.
     """
-
     def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int, optional): The size of the square's side. Defaults to 0.
-
-        Raises:
-            TypeError: If `size` is not an integer.
-            ValueError: If `size` is less than 0.
-        """
+ 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
