5190ba905ec398d3b3a6df079a5d9c83008c5153
SpeedCash <tcravero@gmail.com>
2024-02-05 13:10:51 +0000

    the 1 square updated

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-
 class Square:
+    """A class that defines a square.
+
+    Attributes:
+    size (int): The size of a side of the square.
+    """
 
     def __init__(self, size):
+        """Initialize a new Square.
 
+        Args:
+        size: The size of a side of the square.
+        """
         self.__size = size
