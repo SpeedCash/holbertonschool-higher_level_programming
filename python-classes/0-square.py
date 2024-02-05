8ffb28d4e42547f87971a3c4ca2765e53fd32f30
SpeedCash <tcravero@gmail.com>
2024-02-05 12:53:22 +0000

    the 0 square updated

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""Define a class Square.
-
-This module provides a simple example of a class definition which
-does not do anything yet except defining an empty class named Square.
-"""
-
 
 class Square:
-    """An empty class that defines a square."""
+
     pass
