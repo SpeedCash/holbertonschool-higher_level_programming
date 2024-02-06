95c55726179d26996e5dfa2f72c73bac226b9ea6
SpeedCash <tcravero@gmail.com>
2024-02-06 08:18:16 +0000

    the 0 square file updated

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 """
-This module defines the Square class.
+This module defines a Square class.
 The Square class is an empty class that defines a square.
-No attributes or methods are defined in this version.
 """
 
 
 class Square:
-    """
-    An empty class that defines a square.
-    
-    Attributes:
-    None
-    
-    Methods:
-    None
-    """
+    """An empty class that defines a square."""
     pass
