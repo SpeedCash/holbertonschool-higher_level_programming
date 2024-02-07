4cfa5c678522b1880c7425060d8f787354701546
SpeedCash <tcravero@gmail.com>
2024-02-07 17:44:11 +0000

    the 3 square file updated

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 
 class Square:
-    """Classe Square qui définit un carré."""
+    """Classe Square qui définit un carré avec une taille spécifique.
 
-    def __init__(self, size=0):
-        """
-        Initialise une nouvelle instance de la classe Square.
+    Attributs:
+        __size (int): La taille du carré, un détail privé.
+    """
+
+    def __init__(self, size):
+        """Initialise une nouvelle instance de Square.
 
         Args:
-            size (int): La taille du côté du carré, valeur par défaut de 0.
-        Raises:
-            TypeError: Si size n'est pas un entier.
-            ValueError: Si size est inférieur à 0.
+            size (int): La taille du carré.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """
-        Calcule et retourne l'aire du carré.
-
-        Returns:
-            L'aire du carré.
-        """
-        return self.__size ** 2
