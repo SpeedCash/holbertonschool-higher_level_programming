6dfa2f678e57e3f25c6d50b92c08bf75603f8138
SpeedCash <tcravero@gmail.com>
2024-02-08 11:45:46 +0000

    the 1 square file updated

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python3
+"""
+Module 1-square
+Defines a class Square with a private instance attribute size
+"""
 
 
 class Square:
-    """Classe Square qui définit un carré avec un attribut privé size."""
+    """Defines a square by its size
 
+    Attributes:
+    size (int): The size of a side of the square
+    """
     def __init__(self, size):
         """
-        Initialise une nouvelle instance de la classe Square.
+        Initializes the square
 
         Args:
-            size (int): La taille du côté du carré.
+        size (int): The size of a side of square, no type/value verification
         """
         self.__size = size
+
+
+if __name__ == "__main__":
+    Square = __import__('1-square').Square
+
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
