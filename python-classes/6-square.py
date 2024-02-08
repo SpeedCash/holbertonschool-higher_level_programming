beb38a60c78a4bd686a8386599309a970ee135b1
SpeedCash <tcravero@gmail.com>
2024-02-08 12:02:15 +0000

    the 6 square file updated

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,45 +1,42 @@
 #!/usr/bin/python3
 """
-This module defines a class Square that models a geometric square with
-properties such as size and position, and methods to calculate its area and
-display it on the console.
+Module 6-square
+Defines a class Square with private instance attributes size and position,
+with their getters and setters, and methods to calculate\
+    its area and print the square.
 """
 
+
 class Square:
-    """
-    A Square class that defines a square by its size and position, offering
-    functionalities to calculate its area and print its representation using
-    the '#' character.
-    """
+    """Defines a square by its size and position, with validation."""
     def __init__(self, size=0, position=(0, 0)):
         """
-        Initialize a new Square instance with optional size and position.
-        
+        Initializes the square
+
         Args:
-            size (int): The size of the square's side. Defaults to 0.
-            position (tuple): The square's position as a tuple of 2 positive integers.
+        size (int, optional): The size of a side of the square. Defaults to 0.
+        position (tuple, optional): The position of the square\
+            as a tuple of two integers.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """
-        Retrieve the size of the square.
-        """
+        """Retrieves the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
         """
-        Set the size of the square, including validation checks.
-        
+        Sets the size of the square, with validation.
+
         Args:
-            value (int): The new size of the square's side.
-        
+        value (int): The new size of the square.
+
         Raises:
-            TypeError: If the size is not an integer.
-            ValueError: If the size is less than 0.
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -49,21 +46,19 @@ class Square:
 
     @property
     def position(self):
-        """
-        Retrieve the position of the square.
-        """
+        """Retrieves the position of the square."""
         return self.__position
 
     @position.setter
     def position(self, value):
         """
-        Set the position of the square, including validation checks.
-        
+        Sets the position of the square, with validation.
+
         Args:
-            value (tuple): The new position of the square.
-        
+        value (tuple): The new position of the square.
+
         Raises:
-            TypeError: If the position is not a tuple of 2 positive integers.
+        TypeError: If position is not a tuple of 2 positive integers.
         """
         if not isinstance(value, tuple) or len(value) != 2 \
            or not all(isinstance(num, int) and num >= 0 for num in value):
@@ -71,23 +66,35 @@ class Square:
         self.__position = value
 
     def area(self):
-        """
-        Calculate the area of the square.
-        
-        Returns:
-            The area of the square.
-        """
-        return self.__size ** 2
+        """Returns the current square area."""
+        return self.size ** 2
 
     def my_print(self):
-        """
-        Print the square with the '#' character to the stdout.
-        Takes into account the position of the square.
-        """
-        if self.__size == 0:
+        """Prints square with character '#' or an empty line \
+        if size is 0, considering its position."""
+        if self.size == 0:
             print()
             return
-        for _ in range(self.__position[1]):
+        for _ in range(self.position[1]):
             print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        for _ in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
+
+
+if __name__ == "__main__":
+    Square = __import__('6-square').Square
+
+    my_square_1 = Square(3)
+    my_square_1.my_print()
+
+    print("--")
+
+    my_square_2 = Square(3, (1, 1))
+    my_square_2.my_print()
+
+    print("--")
+
+    my_square_3 = Square(3, (3, 0))
+    my_square_3.my_print()
+
+    print("--")
