3ddcc73d33a7dfd0909930d19756b49d729a5381
SpeedCash <tcravero@gmail.com>
2024-02-09 11:48:38 +0000

    the 0 rectangle file updated

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-    This module defines an empty class Rectangle that defines a rectangle.
+This module defines an empty class Rectangle that defines a rectangle.
 """
 
 
 class Rectangle:
 
-    """
-    An empty class that defines a rectangle.
-    """
 
-    pass
+"""
+An empty class that defines a rectangle.
+"""
+
+pass
