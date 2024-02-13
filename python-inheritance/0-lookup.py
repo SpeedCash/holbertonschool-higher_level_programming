f276461a3ab7bc297aee76a4bc914bd8f01d8bb7
SpeedCash <tcravero@gmail.com>
2024-02-13 08:48:49 +0000

    the 0 lookup file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """Return the list of available attributes and methods of an object.
+
+    Args:
+        obj (Any): The object to inspect.
+
+    Returns:
+        list: A list of strings, each string being an attribute or method name of `obj`.
+    """
+    return dir(obj)
