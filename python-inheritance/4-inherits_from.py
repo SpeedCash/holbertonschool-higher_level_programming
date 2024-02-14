375cde082df48908935f841eb0628415d1e6809d
SpeedCash <tcravero@gmail.com>
2024-02-14 08:51:39 +0000

    the 4 inherit from file

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """Check if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to check against.
+
+    Returns:
+        bool: True if obj is an instance of a class that inherited from
+        a_class, otherwise False. It returns False if obj is an instance of
+        a_class itself.
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
