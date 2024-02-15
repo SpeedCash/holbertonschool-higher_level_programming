3da1baa3e01687c7e60f2b9aa94d5906ddfa418e
SpeedCash <tcravero@gmail.com>
2024-02-15 12:27:06 +0000

    the 3 to json string file updated

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-This module contains a function that returns the JSON representation of an object
-(string). It demonstrates the use of the json module in Python to serialize objects.
+This module contains a function that returns the JSON representation\
+    of an object
+(string). It demonstrates the use of the json module in\
+    Python to serialize objects.
 """
 
 import json
 
+
 def to_json_string(my_obj):
     """Return the JSON representation of an object (string).
-    
+
     Args:
         my_obj: The object to serialize.
-        
+
     Returns:
         str: The JSON representation of my_obj.
     """
