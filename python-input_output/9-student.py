f2efeb657544f6b6a761b7181ce48ad2055731f6
SpeedCash <tcravero@gmail.com>
2024-02-15 13:00:52 +0000

    the 9 student file

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -23,19 +23,3 @@ class Student:
     def to_json(self):
         """Retrieves a dictionary representation of a Student instance."""
         return self.__dict__
-
-
-if __name__ == "__main__":
-    # This block is for demonstration and testing.
-    # It will not be executed when importing this script as a module.
-    Student = __import__('9-student').Student
-
-    students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-    for student in students:
-        j_student = student.to_json()
-        print(type(j_student))
-        print(j_student['first_name'])
-        print(type(j_student['first_name']))
-        print(j_student['age'])
-        print(type(j_student['age']))
