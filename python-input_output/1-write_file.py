7276376355365c0ec42ffc82828c09f4c17c63c2
SpeedCash <tcravero@gmail.com>
2024-02-15 09:46:07 +0000

    the 1 write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -13,9 +13,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
-
-
-# Exemple d'utilisation
-if __name__ == "__main__":
-    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-    print(nb_characters)
