0b598f32bb505b9b05d7317ee1aea59ff9754b95
SpeedCash <tcravero@gmail.com>
2024-02-15 11:56:28 +0000

    the 1 my list updated

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,11 +10,5 @@ class MyList(list):
     print_sorted, qui affiche la liste dans un ordre croissant sans modifier
     la liste originale.
     """
-
     def print_sorted(self):
-        """
-        Imprime les éléments de la liste dans un ordre croissant.
-        Cette méthode crée une nouvelle liste triée pour l'affichage,
-        laissant la liste originale inchangée.
-        """
         print(sorted(self))
