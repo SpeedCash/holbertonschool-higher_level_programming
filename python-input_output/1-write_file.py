4431ff57e25524123e85a678d1b6b63a5818048b
SpeedCash <tcravero@gmail.com>
2024-02-15 09:44:50 +0000

    the 1 write file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    """Écrit une chaîne dans un fichier texte (UTF-8) et retourne le nombre de
+    caractères écrits. Crée le fichier s'il n'existe pas, écrase s'il existe.
+
+    Args:
+        filename (str): Nom du fichier à écrire.
+        text (str): Texte à écrire dans le fichier.
+
+    Returns:
+        int: Nombre de caractères écrits.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
+
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
