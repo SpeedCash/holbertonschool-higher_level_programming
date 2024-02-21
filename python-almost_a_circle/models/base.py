9df70a6dc5854ea786c34f308551e6d939213e03
SpeedCash <tcravero@gmail.com>
2024-02-21 09:56:42 +0000

    the base file updated

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,30 +1,45 @@
 #!/usr/bin/python3
 """Module base.py
-This module defines the Base class for the project 'Almost a Circle'.
-The Base class serves as a foundation for all other classes, managing
-the id attribute to avoid code duplication.
+This module defines the Base class for all other classes in the project.
+The Base class aims to manage id attribute in all future classes and to
+avoid duplicating the same code.
 """
+
+import json
 
 
 class Base:
-    """Defines the Base class.
-    
+    """A base class for all models.
+
     Attributes:
-        __nb_objects (int): A private class attribute to track the number
-                            of objects and to assign new ids.
+        __nb_objects (int): A private class attribute that keeps track of the
+                            number of instantiated Bases (if no id is given).
     """
-    
     __nb_objects = 0
 
     def __init__(self, id=None):
         """Initializes a new Base instance.
 
         Args:
-            id (int): The id of the instance. If id is None, __nb_objects
-                      is incremented and assigned as the id.
+            id (int): The id of the new Base instance. If None, __nb_objects
+                      is incremented and used as the new id.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list of dict): A list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
