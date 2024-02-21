7d83210f595f47b2c049d37e1fa6bcf3a3015528
SpeedCash <tcravero@gmail.com>
2024-02-21 08:43:50 +0000

    the square file updated

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,9 +5,10 @@ Defines the Square class that inherits from Rectangle.
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Defines the Square class that inherits from Rectangle."""
-    
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes a new Square instance."""
         super().__init__(size, size, x, y, id)
@@ -15,23 +16,25 @@ class Square(Rectangle):
     @property
     def size(self):
         """Gets the size of the square."""
-        return self.width  # or self.height, since width == height for Square
+        return self.width
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square.
-        
-        Args:
-            value (int): The new size of the square.
-            
-        Raises:
-            TypeError: If 'value' is not an integer.
-            ValueError: If 'value' is less than or equal to 0.
-        """
-        # Utilise les validateurs de la classe Rectangle pour width et height
+        """Sets the size of the square, adjusting width and height."""
         self.width = value
         self.height = value
 
     def __str__(self):
         """Returns the formatted string representation of the Square."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        """Updates attributes of the Square."""
+        if args and len(args) > 0:
+            attrs = ['id', 'size', 'x', 'y']
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
