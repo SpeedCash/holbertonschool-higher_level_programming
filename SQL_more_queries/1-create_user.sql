08a7f2a8839e54a5da730116360d31498169a10a
SpeedCash <tcravero@gmail.com>
2024-03-19 17:41:56 +0000

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
--- Create the user with specified privileges and password, without failing if it exists
+-- Créer l'utilisateur avec un mot de passe, sans échouer s'il existe déjà
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Accorder tous les privilèges sur toutes les bases de données et les tables avec l'option GRANT
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Rafraîchir les privilèges pour s'assurer que les changements prennent effet immédiatement
 FLUSH PRIVILEGES;
