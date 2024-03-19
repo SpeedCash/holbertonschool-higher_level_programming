318535dedf3826a048ce854c2441510cde6acd89
SpeedCash <tcravero@gmail.com>
2024-03-19 12:31:54 +0000

    Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 12-no_genre.sql)

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists genres and the number of shows linked to each in the hbtn_0d_tvshows database
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY COUNT(tv_show_genres.genre_id) DESC;
