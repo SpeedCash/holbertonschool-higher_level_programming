5af56c8dc5f181863fe737b1c51c5c38528099e7
SpeedCash <tcravero@gmail.com>
2024-03-20 10:58:56 +0000

    scripts corrected...

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,4 @@
 -- Script to list all privileges of users 'user_0d_1' and 'user_0d_2' on MySQL server at localhost
-
-/*
-    Task: List all privileges of the MySQL users user_0d_1 and user_0d_2 on the server (localhost).
-*/
-
 -- Ensure the users exist before listing privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,4 @@
 -- Script to create MySQL server user user_0d_1 with all privileges
-
-/*
-    Task: Create the MySQL server user user_0d_1 with all privileges and a set password.
-          If the user already exists, the script should not fail.
-*/
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
