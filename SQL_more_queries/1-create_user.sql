cb9e0afb7a70e0ad94cc7558daa10c6969f951b7
SpeedCash <tcravero@gmail.com>
2024-03-20 10:54:37 +0000

    task 1 corrected

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,10 @@
--- Créer l'utilisateur avec un mot de passe, sans échouer s'il existe déjà
+-- Script to create MySQL server user user_0d_1 with all privileges
+
+/*
+    Task: Create the MySQL server user user_0d_1 with all privileges and a set password.
+          If the user already exists, the script should not fail.
+*/
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Accorder tous les privilèges sur toutes les bases de données et les tables avec l'option GRANT
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Rafraîchir les privilèges pour s'assurer que les changements prennent effet immédiatement
 FLUSH PRIVILEGES;
